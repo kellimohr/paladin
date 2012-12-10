ca17d02a445bb857ce54a5472dabd94f07386bbe
Kelli Mohr <kmohr@fulcrum.net>
2012-12-09 16:42:46 -0800

    Finished Paladin Class and tests

--- spec/paladin_spec.rb
+++ spec/paladin_spec.rb
@@ -2,28 +2,23 @@ require File.expand_path(File.dirname(__FILE__) + '/spec_helper')
 
 describe "Paladin" do
 
-  	before :each do
+  before :each do
     @file = Paladin.new("test.txt")
 	end
 
-  	it "should open the file and verify a file path exists" do
+  it "should open the file and verify a file path exists" do
 		@file.should be_an_instance_of Paladin
 	end
 
 	it "should accept and read a file" do
-    	pending
-  	end
+    	@file.verify_files
+  end
 
-  	it "should queue missing files" do
-    	@file.missing_files("missing_test.txt").should include "missing_test.txt"
-  	end
+  it "should create paladin.log" do
+    File.exists?("@file.missing_files") == true
+  end
 
-  	it "should queue exisiting files" do
-    	@file.missing_files("test.txt").should include "test.txt"
-  	end
-
-  	it "should print missing files" do
-    	@file.missing_files("missing_test.txt")
-    	@file.get_missing_files.should eq "missing_test.txt"
-  	end
+  it "should print paladin.log" do
+    @file.print_log.should eq ""
+  end
 end
